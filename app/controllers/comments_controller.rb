088a3bf1efaf139866567a4692ffd89ae49323c1
Lucio Jimenez <lucio_diaz@hotmail.com>
2021-10-30 03:12:15 -0500

    Ayax call working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,10 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    
     respond_to do |format|
       format.js
-      #format.html {}
     end
-    #redirect_to post_path(@post)
   end
 
   private
